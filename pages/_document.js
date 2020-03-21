94a79846b44d8df96eb06c5898f73017545bc130
Selcuk <devonukselcuk@gmail.com>
2020-03-21 17:26:42 +0300

    add favicon and apple-touch-icon

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../src/theme";
 
 class MyDocument extends Document {
 	static async getInitialProps (ctx) {
@@ -10,6 +11,16 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
+					<link rel="icon" sizes="192x192" href="/favicon/apple-touch-icon.png" />
+					<link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+					<link rel="icon" sizes="32x32" type="image/png" href="/favicon/favicon.png" />
+					<meta
+						name="viewport"
+						content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+						key="viewport"
+					/>
+					{/* PWA primary color */}
+					<meta name="theme-color" content={theme.palette.primary.main} />
 					<link href="https://fonts.googleapis.com/css?family=Lato:400,700&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
