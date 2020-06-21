a127020fee4f00ef307797bae4b88afe62a58993
Selcuk <devonukselcuk@gmail.com>
2020-06-21 11:50:52 +0300

    add service worker with next-offline

--- routes/api/discoverRoutes.js
+++ routes/api/discoverRoutes.js
@@ -5,37 +5,37 @@ const axios = require("axios");
 //* discover movie
 
 router.get("/movie", async (req, res) => {
-	try {
-		let url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&include_adult=false`;
-		const keysArray = Object.keys(req.query);
+    try {
+        let url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}&include_adult=false`;
+        const keysArray = Object.keys(req.query);
 
-		keysArray.forEach((cur) => {
-			url = `${url}&${cur}=${req.query[cur]}`;
-		});
+        keysArray.forEach((cur) => {
+            url = `${url}&${cur}=${req.query[cur]}`;
+        });
 
-		const response = await axios.get(url);
+        const response = await axios.get(url);
 
-		res.send(response.data);
-	} catch (error) {
-		res.send("something went wrong");
-	}
+        res.send(response.data);
+    } catch (error) {
+        res.send("something went wrong");
+    }
 });
 
 router.get("/show", async (req, res) => {
-	try {
-		let url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&include_adult=false`;
-		const keysArray = Object.keys(req.query);
+    try {
+        let url = `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.TMDB_API_KEY}&include_adult=false`;
+        const keysArray = Object.keys(req.query);
 
-		keysArray.forEach((cur) => {
-			url = `${url}&${cur}=${req.query[cur]}`;
-		});
+        keysArray.forEach((cur) => {
+            url = `${url}&${cur}=${req.query[cur]}`;
+        });
 
-		const response = await axios.get(url);
+        const response = await axios.get(url);
 
-		res.send(response.data);
-	} catch (error) {
-		res.send("something went wrong");
-	}
+        res.send(response.data);
+    } catch (error) {
+        res.send("something went wrong");
+    }
 });
 
 module.exports = router;
