1518342293e9d5069d5d5c30c1d690c6053080bb
Selcuk <devonukselcuk@gmail.com>
2020-06-25 08:50:15 +0300

    add optimized images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
+const withPlugins = require("next-compose-plugins");
+//const withFonts = require("next-fonts");
+const optimizedImages = require("next-optimized-images");
+// const dev = true;
 const nextEnv = require("next-env");
 require("dotenv").config();
+const dev = process.env.NEXT_STATIC_PRODUCTION !== "production";
 
 const withNextEnv = nextEnv();
 
-module.exports = withNextEnv({
-    // Your Next.js config.
-});
+module.exports = withPlugins([
+    withNextEnv,
+    [
+        optimizedImages,
+        {
+            imagesPublicPath: dev
+                ? "/_next/static/images/"
+                : "https://assets.darklyst.com/images"
+        }
+    ]
+    //withCSS,
+    //withFonts
+]);
