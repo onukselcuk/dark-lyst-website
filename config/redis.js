aa058e379c2e62aad3775f5adbb42bc41367c307
Selçuk Onuk <devonukselcuk@gmail.com>
2020-10-11 12:35:47 +0300

    change redis url

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,5 @@
 const redis = require("async-redis");
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({url:process.env.HEROKU_REDIS_URL});
 const { logger } = require("./logger");
 
 redisClient.on("connect", () => {
