e17f5e7e2fb603b4c74b8886d622bc726eb140a0
Selcuk Onuk <devonukselcuk@gmail.com>
2023-04-10 12:55:10 +0300

    Update redis url

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,5 @@
 const redis = require("async-redis");
-const redisClient = redis.createClient({url:process.env.HEROKU_REDIS_URL});
+const redisClient = redis.createClient({url:process.env.REDIS_URL});
 const { logger } = require("./logger");
 
 redisClient.on("connect", () => {
